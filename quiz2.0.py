b6d037fe792ce6e88d7025df0d02d7fc1bc0f07f
Serdesiyon <serdesiyon.tsegaye@aastustudent.edu.et>
2024-10-06 19:11:14 +0300

    fully done for cmd

--- quiz2.0.py
+++ quiz2.0.py
@@ -1,5 +1,6 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+import json, time
 
 
 template = """
@@ -13,21 +14,49 @@ template = """
                              {{
                                         "question": "What is the capital of France?",
                                         "choices": ["London", "Berlin", "Paris", "Madrid"],
-                                        "correct_answer": "Paris",
+                                        "correct_answer": "Paris"
                                  
                                 }} 
           """
 
-model = OllamaLLM(model='gemma:2b')
+model = OllamaLLM(model='phi3:latest')
 
 prompt = ChatPromptTemplate.from_template(template)
 
 chain = prompt | model
-def test() -> None:
+
+
+topic = input('Type your topic: ')
+print('you\'ve 30 second for each question')
+
+
+def test(time_limit=10):
         score = 0
-        topic = input('Type your topic: ') 
-        questions = chain.invoke({"topic": topic, "score": score})
-        print(questions)
+        for _ in range(10):
+                question =json.loads(chain.invoke({"topic": topic, "score": score}))
+                
+                print(question['question'])
+                start_time = int(time.time())
+                
+                for n, choices in enumerate(question['choices'],1):
+                        print(n, choices)
+                
+                user_ans = int(input())
+                answer_time = int(time.time())
+        
+                last_option = len(question['choices'])
+                
+                if not 0 < user_ans <= last_option:
+                        user_ans = int(input(f'choose between 1 - {last_option} -> '))
+                
+                if question['choices'][user_ans-1] == question['correct_answer'] and (answer_time - start_time < time_limit):
+                        score += 1
+                else:
+                        print(f'correct: {question['correct_answer']}')
+                if answer_time - start_time > time_limit:
+                        print('Time Exceeded')
+        print(f"you\'ve got {score} out of 10")
+
 test()
 
 
